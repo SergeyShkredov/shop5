3cb27331af2a7d24dbb3ec18181fee84b33dc7c7
user19 <user19@mail.uonmap.com>
2022-07-13 16:22:30 +0300

    test

---
+++ src/components/newTest.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: "newTest"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,13 @@
+<template>
+<di></di>
+</template>
+
+<script>
+export default {
+  name: "test"
+}
+</script>
+
+<style scoped>
+
+</style>
